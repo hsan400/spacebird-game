cf8366b9253e511e955423b1f01e42dd10ba616f
hsan400 <hsan400@aucklanduni.ac.nz>
2026-05-21 14:48:44 +1200

    new bg_render

--- bg_renderer.vhd
+++ bg_renderer.vhd
@@ -1,51 +1,78 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
-entity bg_renderer is 
+entity bg_renderer is
     port (
-        pixel_row, pixel_column : in STD_LOGIC_VECTOR(9 DOWNTO 0);
-        scroll_en, reset : in std_logic;
-        red, green, blue : out std_logic;
-    )
+        clk          : in  std_logic;
+        pixel_row    : in  std_logic_vector(9 downto 0);
+        pixel_column : in  std_logic_vector(9 downto 0);
+        scroll_en    : in  std_logic;
+        reset        : in  std_logic;
+        red, green, blue : out std_logic_vector(3 downto 0)
+
+    );
 end bg_renderer;
 
-architecture behaviour of bg_renderer is 
-    signal scroll_x : std_logic_vector(9 downto 0) := (others => '0');
+architecture behaviour of bg_renderer is
+
+    signal scroll_x : unsigned(9 downto 0) := (others => '0');
 
 begin
-    process(scroll_en, reset)
+
+    process(clk, reset)
     begin
-        if (reset = '1') then 
+
+        if reset = '1' then
             scroll_x <= (others => '0');
-        elsif rising_edge(scroll_en) then 
-            scroll_x <= scroll_x + 1;
+
+        elsif rising_edge(clk) then
+
+            if scroll_en = '1' then
+                scroll_x <= scroll_x + 1;
+            end if;
+
         end if;
+
     end process;
 
     process(pixel_row, pixel_column, scroll_x)
-        variable x, y, sx : integer;
-    begin
-        x := conv_integer(pixel_column);
-        y := conv_integer(pixel_row);
-        sx := conv_integer(scroll_x);
 
-        red <= '0';
-        green <= '0';
-        blue <= '1';
+    variable x  : integer;
+    variable y  : integer;
+    variable sx : integer;
 
-        if (((x + sx) mod 63 = 0) and ((y + sx) mod 47 = 0)) then
-            red   <= '1';
-            green <= '1';
-            blue  <= '1';  
-        end if;
+begin
 
-        if (((x + sx/2) mod 97 = 0) and (y mod 83 = 0)) then
-            red   <= '1';
-            green <= '1';
-            blue  <= '0';  
-        end if;
+    x  := to_integer(unsigned(pixel_column));
+    y  := to_integer(unsigned(pixel_row));
+    sx := to_integer(scroll_x);
 
-    end process;
+    -- Blue background
+    red   <= "0000";
+    green <= "0000";
+    blue  <= "1111";
 
-end architecture behaviour;
+    -- White stars (4x4 pixels)
+    if (((x + sx) mod 63 < 4) and
+        ((y + sx) mod 47 < 4)) then
+
+        red   <= "1111";
+        green <= "1111";
+        blue  <= "1111";
+
+    end if;
+
+    -- Yellow stars (3x3 pixels)
+    if (((x + (sx/2)) mod 97 < 3) and
+        (y mod 83 < 3)) then
+
+        red   <= "1111";
+        green <= "1111";
+        blue  <= "0000";
+
+    end if;
+
+end process;
+
+end behaviour;
